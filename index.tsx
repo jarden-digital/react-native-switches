b1c951a8cc0b6c6c0ffc740a38b036177b0bc3ce
Jeremy Farnault <haskkor@gmail.com>
2018-06-11 09:03:39 +1200

    Working on the second switch

--- index.tsx
+++ index.tsx
@@ -15,7 +15,7 @@ export type IProps = {
   colorTextOff?: string
   colorTextOn?: string
   defaultValue?: boolean
-  easingFunction?: any // fixme any
+  easingFunction?: () => void
   onChange?: (switchOn: boolean) => void
   shape: Shapes
   sliderHeight?: number
@@ -36,21 +36,29 @@ export type IState = {
   switchOn: boolean
 }
 
-const defaultButtonOffset = 2
+const defaultButtonOffsetLine = 0
+const defaultButtonOffsetPill = 2
 const defaultButtonSize = 32
 const defaultButtonColor = '#FFF'
 const defaultColorSwitchOff = '#D3D5DA'
 const defaultColorSwitchOn = '#96CBCE'
-const defaultColorTextOff = '#000' // todo put base color avec 60% opacity
-const defaultColorTextOn = '#FFF'
+const defaultColorTextOffLine = '#7BCDD0'
+const defaultColorTextOnLine = '#E8EAEC'
+const defaultColorTextOffPill = '#445878'
+const defaultColorTextOnPill = '#FFF'
 const defaultDuration = 300
 const defaultEasingFunction = easeQuadIn
-const defaultSpaceBetween = 8
-const defaultSwitchHeight = 36
-const defaultSwitchWidth = 70
+const defaultSpaceBetweenLine = 20
+const defaultSpaceBetweenPill = 8
+const defaultSwitchHeightLine = 6
+const defaultSwitchWidthLine = 25
+const defaultSwitchHeightPill = 36
+const defaultSwitchWidthPill = 70
 const defaultTextFont = 'courier'
-const defaultTextOff = 'off'
-const defaultTextOn = 'on'
+const defaultTextOffLine = 'No'
+const defaultTextOnLine = 'Yes'
+const defaultTextOffPill = 'off'
+const defaultTextOnPill = 'on'
 const defaultTextSize = 16
 
 class Switches extends React.PureComponent<IProps, IState> {
@@ -69,27 +77,81 @@ class Switches extends React.PureComponent<IProps, IState> {
   }
 
   renderLineSwitch = () => {
-    return null
+    const buttonOffsetLeft = this.props.buttonOffsetLeft || defaultButtonOffsetLine
+    const buttonOffsetRight = this.props.buttonOffsetRight || defaultButtonOffsetLine
+    const easingFunction = this.props.easingFunction || defaultEasingFunction
+    const duration = this.props.animationDuration || defaultDuration
+    const colorTextOn = this.props.colorTextOn || defaultColorTextOnLine
+    const colorTextOff = this.props.colorTextOff || defaultColorTextOffLine
+    const textFont = this.props.textFont || defaultTextFont
+    const textOn = this.props.textOn || defaultTextOnPill
+    const textOff = this.props.textOff || defaultTextOffPill
+    const sliderHeight = this.props.sliderHeight || defaultSwitchHeightLine
+    const sliderWidth = this.props.sliderWidth || defaultSwitchWidthLine
+    const spaceBetween = this.props.spaceBetween || defaultSpaceBetweenPill
+    return (
+      <Animate
+        show={true}
+        start={{
+          colorNo: this.state.switchOn ? colorTextOff : colorTextOn,
+          colorYes: this.state.switchOn ? colorTextOn : colorTextOff,
+          positionButton: this.state.switchOn ? sliderWidth - buttonOffsetRight : 0 + buttonOffsetLeft,
+          opacityChildren: this.state.switchOn ? 1 : 0
+        }}
+        update={{
+          colorNo: [this.state.switchOn ? colorTextOff : colorTextOn],
+          colorYes: [this.state.switchOn ? colorTextOn : colorTextOff],
+          positionButton: [this.state.switchOn ? sliderWidth - buttonOffsetRight : 0 + buttonOffsetLeft],
+          opacityChildren: this.state.switchOn ? [1] : [0],
+          timing: {duration: duration, ease: easingFunction}
+        }}>
+        {(state: any) => (
+          <View style={{width: '100%'}}>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{fontFamily: textFont, color: state.colorNo, marginRight: spaceBetween / 2}}>
+                {textOff}
+              </Text>
+
+
+
+              <View style={{position: 'relative', paddingRight: 10, paddingLeft: 10}}>
+                <View style={{height: 6, width: 25, backgroundColor: '#E8EAEC', borderRadius: 3}}/>
+                <TouchableOpacity activeOpacity={1} style={[pagesStyles.buttonSwitch, {left: state.positionButton}]}
+                                  onPress={() => this.setState({switchOn: !this.state.switchOn})}/>
+              </View>
+
+
+
+              <Text style={{fontFamily: textFont, color: state.colorYes, marginLeft: spaceBetween / 2}}>
+                {textOn}
+              </Text>
+            </View>
+            <View style={{opacity: state.opacityChildren}}>
+            </View>
+          </View>
+        )}
+      </Animate>
+    )
   }
 
   renderPillSwitch = () => {
-    const onColor = this.props.colorSwitchOn || defaultColorSwitchOn
-    const offColor = this.props.colorSwitchOff || defaultColorSwitchOff
-    const leftPosition = this.props.buttonOffsetLeft || defaultButtonOffset
-    const sliderWidth = this.props.sliderWidth || defaultSwitchWidth
-    const sliderHeight = this.props.sliderHeight || defaultSwitchHeight
+    const buttonOffSetRight = this.props.buttonOffsetRight || defaultButtonOffsetPill
     const buttonSize = this.props.buttonWidth || defaultButtonSize
-    const buttonOffSetRight = this.props.buttonOffsetRight || defaultButtonOffset
+    const leftPosition = this.props.buttonOffsetLeft || defaultButtonOffsetPill
+    const offColor = this.props.colorSwitchOff || defaultColorSwitchOff
+    const onColor = this.props.colorSwitchOn || defaultColorSwitchOn
+    const sliderHeight = this.props.sliderHeight || defaultSwitchHeightPill
+    const sliderWidth = this.props.sliderWidth || defaultSwitchWidthPill
     const rightPosition = sliderWidth - buttonSize - buttonOffSetRight
     const duration = this.props.animationDuration || defaultDuration
     const easingFunction = this.props.easingFunction || defaultEasingFunction
     const textFont = this.props.textFont || defaultTextFont
     const textSize = this.props.textSize || defaultTextSize
-    const colorTextOn = this.props.colorTextOn || defaultColorTextOn
-    const colorTextOff = this.props.colorTextOff || defaultColorTextOff
-    const textOn = this.props.textOn || defaultTextOn
-    const textOff = this.props.textOff || defaultTextOff
-    const spaceBetween = this.props.spaceBetween || defaultSpaceBetween
+    const colorTextOn = this.props.colorTextOn || defaultColorTextOnPill
+    const colorTextOff = this.props.colorTextOff || defaultColorTextOffPill
+    const textOn = this.props.textOn || defaultTextOnPill
+    const textOff = this.props.textOff || defaultTextOffPill
+    const spaceBetween = this.props.spaceBetween || defaultSpaceBetweenPill
     const buttonColor = this.props.buttonColor || defaultButtonColor
     return (<Animate
       show={true}
@@ -101,7 +163,7 @@ class Switches extends React.PureComponent<IProps, IState> {
       update={{
         color: [this.state.switchOn ? onColor : offColor],
         positionButton: [this.state.switchOn ? rightPosition : leftPosition],
-        opacityChildren: this.state.switchOn ? [1] : [0],
+        opacityChildren: [this.state.switchOn ? 1 : 0],
         timing: {duration: duration, ease: easingFunction}
       }}>
       {(state: any) => (
@@ -170,41 +232,6 @@ let styles = StyleSheet.create({
 })
 
 // SKINNY ONE
-
-//   <Animate
-// show={true}
-// start={{
-//   colorNo: !this.state.switchOn ? '#7BCDD0' : '#E8EAEC',
-//     colorYes: this.state.switchOn ? '#7BCDD0' : '#E8EAEC',
-//     positionButton: this.state.switchOn ? 25 : 0,
-//     opacityPep: this.state.switchOn ? 1 : 0
-// }}
-// update={{
-//   colorNo: [!this.state.switchOn ? '#7BCDD0' : '#E8EAEC'],
-//     colorYes: [this.state.switchOn ? '#7BCDD0' : '#E8EAEC'],
-//     positionButton: [this.state.switchOn ? 25 : 0],
-//     opacityPep: [this.state.switchOn ? 1 : 0],
-//     timing: {duration: 300, ease: easeQuadIn}
-// }}>
-// {(state: any) => (
-//   <View style={{width: '100%'}}>
-//     <View style={[pagesStyles.section, {alignItems: 'flex-end', width: '100%'}]}>
-//       <View style={[pagesStyles.row, {justifyContent: 'center', alignItems: 'center'}]}>
-//         <Text style={{fontFamily: grid.fontBold, color: state.colorNo, marginRight: 10}}>No</Text>
-//         <View style={{position: 'relative', paddingRight: 10, paddingLeft: 10}}>
-//           <View style={{height: 6, width: 25, backgroundColor: '#E8EAEC', borderRadius: 3}}/>
-//           <TouchableOpacity activeOpacity={1} style={[pagesStyles.buttonSwitch, {left: state.positionButton}]}
-//                             onPress={() => this.setState({switchOn: !this.state.switchOn})}/>
-//         </View>
-//         <Text style={{fontFamily: grid.fontBold, color: state.colorYes, marginLeft: 10}}>Yes</Text>
-//       </View>
-//     </View>
-//     <View style={[pagesStyles.section, {opacity: state.opacityPep}]}>
-//     </View>
-//   </View>
-// )}
-// </Animate>
-
 
 // buttonSwitch: {
 //   position: 'absolute',
