1d5f89b7b370eefabfbaf8e3e76eaa0017cf81b1
Jeremy Farnault <haskkor@gmail.com>
2018-06-12 09:05:07 +1200

    Component done. Need testing

--- index.tsx
+++ index.tsx
@@ -10,14 +10,16 @@ export type IProps = {
   buttonOffsetLeft?: number
   buttonOffsetRight?: number
   buttonWidth?: number
+  colorSwitch?: string
   colorSwitchOff?: string
   colorSwitchOn?: string
   colorTextOff?: string
   colorTextOn?: string
-  defaultValue?: boolean
+  disabled?: boolean
   easingFunction?: () => void
-  onChange?: (switchOn: boolean) => void
+  onChange: () => void
   shape: Shapes
+  showText?: boolean
   sliderHeight?: number
   sliderWidth?: number
   spaceBetween?: number
@@ -25,6 +27,7 @@ export type IProps = {
   textOff?: string
   textOn?: string
   textSize?: number
+  value: boolean
 }
 
 enum Shapes {
@@ -32,22 +35,25 @@ enum Shapes {
   line = 'line'
 }
 
-export type IState = {
-  switchOn: boolean
-}
+export type IState = {}
 
 const defaultButtonOffsetLine = 0
 const defaultButtonOffsetPill = 2
-const defaultButtonSize = 32
-const defaultButtonColor = '#FFF'
+const defaultButtonSizeLine = 20
+const defaultButtonSizePill = 32
+const defaultButtonColorLine = '#96CBCE'
+const defaultButtonColorPill = '#FFF'
+const defaultColorSwitch = '#E8EAEC'
 const defaultColorSwitchOff = '#D3D5DA'
 const defaultColorSwitchOn = '#96CBCE'
 const defaultColorTextOffLine = '#7BCDD0'
 const defaultColorTextOnLine = '#E8EAEC'
 const defaultColorTextOffPill = '#445878'
 const defaultColorTextOnPill = '#FFF'
+const defaultDisabledValue = false
 const defaultDuration = 300
 const defaultEasingFunction = easeQuadIn
+const defaultShowText = true
 const defaultSpaceBetweenLine = 20
 const defaultSpaceBetweenPill = 8
 const defaultSwitchHeightLine = 6
@@ -65,66 +71,73 @@ class Switches extends React.PureComponent<IProps, IState> {
 
   constructor(props: IProps) {
     super(props)
-    this.state = {
-      switchOn: this.props.defaultValue || true
-    }
     this.handleSwitch = this.handleSwitch.bind(this)
   }
 
   handleSwitch = () => {
-    if (this.props.onChange) this.props.onChange(!this.state.switchOn)
-    this.setState({switchOn: !this.state.switchOn})
+    this.props.onChange()
   }
 
   renderLineSwitch = () => {
     const buttonOffsetLeft = this.props.buttonOffsetLeft || defaultButtonOffsetLine
     const buttonOffsetRight = this.props.buttonOffsetRight || defaultButtonOffsetLine
     const easingFunction = this.props.easingFunction || defaultEasingFunction
+    const buttonSize = this.props.buttonWidth || defaultButtonSizeLine
     const duration = this.props.animationDuration || defaultDuration
     const colorTextOn = this.props.colorTextOn || defaultColorTextOnLine
+    const disabled = this.props.disabled || defaultDisabledValue
     const colorTextOff = this.props.colorTextOff || defaultColorTextOffLine
     const textFont = this.props.textFont || defaultTextFont
-    const textOn = this.props.textOn || defaultTextOnPill
-    const textOff = this.props.textOff || defaultTextOffPill
+    const textOn = this.props.textOn || defaultTextOnLine
+    const textOff = this.props.textOff || defaultTextOffLine
     const sliderHeight = this.props.sliderHeight || defaultSwitchHeightLine
     const sliderWidth = this.props.sliderWidth || defaultSwitchWidthLine
-    const spaceBetween = this.props.spaceBetween || defaultSpaceBetweenPill
+    const spaceBetween = this.props.spaceBetween || defaultSpaceBetweenLine
+    const buttonColor = this.props.buttonColor || defaultButtonColorLine
+    const colorSwitch = this.props.colorSwitch || defaultColorSwitch
+    const showText = this.props.showText || defaultShowText
     return (
       <Animate
         show={true}
         start={{
-          colorNo: this.state.switchOn ? colorTextOff : colorTextOn,
-          colorYes: this.state.switchOn ? colorTextOn : colorTextOff,
-          positionButton: this.state.switchOn ? sliderWidth - buttonOffsetRight : 0 + buttonOffsetLeft,
-          opacityChildren: this.state.switchOn ? 1 : 0
+          colorNo: this.props.value ? colorTextOff : colorTextOn,
+          colorYes: this.props.value ? colorTextOn : colorTextOff,
+          positionButton: this.props.value ? sliderWidth - buttonOffsetRight : 0 + buttonOffsetLeft,
+          opacityChildren: this.props.value ? 1 : 0
         }}
         update={{
-          colorNo: [this.state.switchOn ? colorTextOff : colorTextOn],
-          colorYes: [this.state.switchOn ? colorTextOn : colorTextOff],
-          positionButton: [this.state.switchOn ? sliderWidth - buttonOffsetRight : 0 + buttonOffsetLeft],
-          opacityChildren: this.state.switchOn ? [1] : [0],
+          colorNo: [this.props.value ? colorTextOff : colorTextOn],
+          colorYes: [this.props.value ? colorTextOn : colorTextOff],
+          positionButton: [this.props.value ? sliderWidth - buttonOffsetRight : 0 + buttonOffsetLeft],
+          opacityChildren: this.props.value ? [1] : [0],
           timing: {duration: duration, ease: easingFunction}
         }}>
         {(state: any) => (
           <View style={{width: '100%'}}>
             <View style={{justifyContent: 'center', alignItems: 'center'}}>
-              <Text style={{fontFamily: textFont, color: state.colorNo, marginRight: spaceBetween / 2}}>
+              {showText && <Text style={{fontFamily: textFont, color: state.colorNo, marginRight: spaceBetween / 2}}>
                 {textOff}
-              </Text>
-
-
-
-              <View style={{position: 'relative', paddingRight: 10, paddingLeft: 10}}>
-                <View style={{height: 6, width: 25, backgroundColor: '#E8EAEC', borderRadius: 3}}/>
-                <TouchableOpacity activeOpacity={1} style={[pagesStyles.buttonSwitch, {left: state.positionButton}]}
-                                  onPress={() => this.setState({switchOn: !this.state.switchOn})}/>
+              </Text>}
+              <View style={{position: 'relative', paddingRight: spaceBetween / 2, paddingLeft: spaceBetween / 2}}>
+                <View style={{
+                  height: sliderHeight,
+                  width: sliderWidth,
+                  backgroundColor: colorSwitch,
+                  borderRadius: sliderHeight / 2
+                }}/>
+                <TouchableOpacity activeOpacity={1} disabled={disabled} style={{
+                  left: state.positionButton,
+                  position: 'absolute',
+                  top: (sliderHeight - buttonSize) / 2,
+                  width: buttonSize,
+                  height: buttonSize,
+                  borderRadius: buttonSize / 2,
+                  backgroundColor: buttonColor
+                }} onPress={() => this.handleSwitch()}/>
               </View>
-
-
-
-              <Text style={{fontFamily: textFont, color: state.colorYes, marginLeft: spaceBetween / 2}}>
+              {showText && <Text style={{fontFamily: textFont, color: state.colorYes, marginLeft: spaceBetween / 2}}>
                 {textOn}
-              </Text>
+              </Text>}
             </View>
             <View style={{opacity: state.opacityChildren}}>
             </View>
@@ -136,7 +149,7 @@ class Switches extends React.PureComponent<IProps, IState> {
 
   renderPillSwitch = () => {
     const buttonOffSetRight = this.props.buttonOffsetRight || defaultButtonOffsetPill
-    const buttonSize = this.props.buttonWidth || defaultButtonSize
+    const buttonSize = this.props.buttonWidth || defaultButtonSizePill
     const leftPosition = this.props.buttonOffsetLeft || defaultButtonOffsetPill
     const offColor = this.props.colorSwitchOff || defaultColorSwitchOff
     const onColor = this.props.colorSwitchOn || defaultColorSwitchOn
@@ -144,6 +157,7 @@ class Switches extends React.PureComponent<IProps, IState> {
     const sliderWidth = this.props.sliderWidth || defaultSwitchWidthPill
     const rightPosition = sliderWidth - buttonSize - buttonOffSetRight
     const duration = this.props.animationDuration || defaultDuration
+    const disabled = this.props.disabled || defaultDisabledValue
     const easingFunction = this.props.easingFunction || defaultEasingFunction
     const textFont = this.props.textFont || defaultTextFont
     const textSize = this.props.textSize || defaultTextSize
@@ -152,30 +166,35 @@ class Switches extends React.PureComponent<IProps, IState> {
     const textOn = this.props.textOn || defaultTextOnPill
     const textOff = this.props.textOff || defaultTextOffPill
     const spaceBetween = this.props.spaceBetween || defaultSpaceBetweenPill
-    const buttonColor = this.props.buttonColor || defaultButtonColor
+    const buttonColor = this.props.buttonColor || defaultButtonColorPill
+    const showText = this.props.showText || defaultShowText
     return (<Animate
       show={true}
       start={{
-        color: this.state.switchOn ? onColor : offColor,
-        positionButton: this.state.switchOn ? rightPosition : leftPosition,
-        opacityChildren: this.state.switchOn ? 1 : 0
+        color: this.props.value ? onColor : offColor,
+        positionButton: this.props.value ? rightPosition : leftPosition,
+        opacityChildren: this.props.value ? 1 : 0
       }}
       update={{
-        color: [this.state.switchOn ? onColor : offColor],
-        positionButton: [this.state.switchOn ? rightPosition : leftPosition],
-        opacityChildren: [this.state.switchOn ? 1 : 0],
+        color: [this.props.value ? onColor : offColor],
+        positionButton: [this.props.value ? rightPosition : leftPosition],
+        opacityChildren: [this.props.value ? 1 : 0],
         timing: {duration: duration, ease: easingFunction}
       }}>
       {(state: any) => (
         <View>
           <View style={{width: '100%'}}>
-            <TouchableOpacity activeOpacity={1} onPress={() => this.handleSwitch()}
+            <TouchableOpacity activeOpacity={1} onPress={() => this.handleSwitch()} disabled={disabled}
                               style={{
-                                backgroundColor: state.color, height: sliderHeight, width: sliderWidth,
-                                borderRadius: sliderHeight / 2, position: 'relative', justifyContent: 'center',
+                                backgroundColor: state.color,
+                                height: sliderHeight,
+                                width: sliderWidth,
+                                borderRadius: sliderHeight / 2,
+                                position: 'relative',
+                                justifyContent: 'center',
                                 alignItems: 'center'
                               }}>
-              <View style={{flexDirection: 'row'}}>
+              {showText && <View style={{flexDirection: 'row'}}>
                 <Text
                   style={{fontFamily: textFont, fontSize: textSize, color: colorTextOn, marginRight: spaceBetween / 2}}>
                   {textOn}
@@ -184,7 +203,7 @@ class Switches extends React.PureComponent<IProps, IState> {
                   style={{fontFamily: textFont, fontSize: textSize, color: colorTextOff, marginLeft: spaceBetween / 2}}>
                   {textOff}
                 </Text>
-              </View>
+              </View>}
               <View style={{
                 left: state.positionButton,
                 position: 'absolute',
@@ -231,39 +250,6 @@ let styles = StyleSheet.create({
   }
 })
 
-// SKINNY ONE
-
-// buttonSwitch: {
-//   position: 'absolute',
-//     top: -7,
-//     width: 20,
-//     height: 20,
-//     borderRadius: 20,
-//     backgroundColor: 'rgb(150, 203, 206)'
-// },
-
-
 //   <Switch
 // value={true}
-// onValueChange={(val) => console.log(val)}
-// disabled={false}
-// activeText={'On'}
-// inActiveText={'Off'}
-// circleSize={30}
-// barHeight={1}
-// circleBorderWidth={3}
-// backgroundActive={'green'}
-// backgroundInactive={'gray'}
-// circleActiveColor={'#30a566'}
-// circleInActiveColor={'#000000'}
-// changeValueImmediately={true}
-// renderInsideCircle={() => <CustomComponent />} // custom component to render inside the Switch circle (Text, Image, etc.)
-// changeValueImmediately={true} // if rendering inside circle, change state immediately or wait for animation to complete
-// innerCircleStyle={{ alignItems: "center", justifyContent: "center" }} // style for inner animated circle for what you (may) be rendering inside the circle
-// outerCircleStyle={{}} // style for outer animated circle
-// renderActiveText={false}
-// renderInActiveText={false}
-// switchLeftPx={2} // denominator for logic when sliding to TRUE position. Higher number = more space from RIGHT of the circle to END of the slider
-// switchRightPx={2} // denominator for logic when sliding to FALSE position. Higher number = more space from LEFT of the circle to BEGINNING of the slider
-// switchWidthMultiplier={2} // multipled by the `circleSize` prop to calculate total width of the Switch
 // />
